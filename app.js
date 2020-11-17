5d1d9efe73d5820a1d4245a1ad4233c58f54d739
DDRAGON <davide.0829@gmail.com>
2020-11-17 12:29:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();

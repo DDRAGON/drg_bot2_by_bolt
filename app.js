9e71fb8dbcec21c5d5e93a3c125ea58c77523088
DDRAGON <davide.0829@gmail.com>
2020-11-17 17:25:47 +0900

    機能テスト

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.message('hello', async ({ message, say }) => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 
-  app.client.chat.postMessage({
+  const result = await app.client.chat.postMessage({
     channel: '#davide_test',
     text: `You can introduce yourself in this channel.`
   });
+
+  console.log(result);
 })();
